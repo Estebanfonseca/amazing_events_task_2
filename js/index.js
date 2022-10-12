8a24a4e5839c54318b9b0c2ec5c7bc29cad96931
Estebanfonseca <estebanp57@hotmail.com>
2022-10-11 19:14:57 -0500

    cambio a un solo js

--- js/index.js
+++ js/index.js
@@ -1,22 +1,34 @@
-let cards = document.getElementById("section")
-
-
-for (let i = 0; i < events.length;i++){
-    let carta = document.createElement('div')
-    carta.className='card rounded-4 '
-    carta.innerHTML +=` <img class="p-3 rounded-5" src="${events[i].image}" alt="${events[i].name}" height="250" />
-    <article class="card-body">
-        <h4>${events[i].name}</h4>
-        <p>${events[i].description}</p>
-        <a href="#" class="btn btn-secondary">see more</a>
-    </article>`
-    cards.appendChild(carta)
-}
+let cardHome = document.getElementById("section")
+let cardPast = document.getElementById("sectionPast")
+let cardUpcomming = document.getElementById('section1')
+let fecha = Date.parse(currentDate)
+let past = events.filter(function(date){
+    return Date.parse(date.date) < fecha
+})
+let upcomming = events.filter(function(date){
+    return Date.parse(date.date) > fecha
+})
 
 
 
 
 
 
+function printCards(array,id){
+    for(let i = 0;i< array.length;i++){
+        let carta = document.createElement('div')
+        carta.className ='card rounded-4 '
+        carta.innerHTML+=` <img class="p-3 rounded-5" src="${array[i].image}" alt="${array[i].name}" height="250" />
+        <article class="card-body">
+            <h4>${array[i].name}</h4>
+            <p>${array[i].description}</p>
+            <a href="#" class="btn btn-secondary">see more</a>
+        </article>`
+        id?.appendChild(carta)
+}}
+
+printCards(past,cardPast)
+printCards(upcomming,cardUpcomming)
+printCards(events,cardHome)
 
 
